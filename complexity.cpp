12dbfbf45300c8bf89c2209f79f0ca05c90e6c32
Kinlo Ephriam Tangiri (KET) <kinlotangiri911@gmail.com>
2024-09-16 10:56:09 +0200

    solve the complexity

---
+++ complexity.cpp
@@ -0,0 +1,19 @@
+/*
+Asymptotic Complexity
+
+If we have a function f(x)=8+x^3
+then the asymptotic lower bound is: Ω(x^3)
+
+The asymptotic tight bound Θ is usually the intersection of two functions starting with big O and Omega Ω
+This usually means the function is bounded above and below by the same asymptotic rate
+
+Θ(f(n))=O(f(n))∩Ω(f(n))
+
+Solving the problem 2T(n/2) + cn, where n>=1 and c > 0 is a constant leads to 
+Θ(nlogn)
+Θ(nlogn^2) this is so because transforming this to normal form by applying log rules, this will lead to Θ(2nlogn) which is equivalent to Θ(nlogn)
+
+If we have 11 + x^3 then the upper bound big O(x^3)
+
+
+*/
